73966ae8bd70ce12708f23d7cd106c23725ee464
Alzwimn <53050916+Alzwimn@users.noreply.github.com>
2020-03-10 15:51:29 -0300

    Add files via upload

--- js/page.js
+++ js/page.js
@@ -21,9 +21,31 @@ function aboutSectionAnimation(){
     para.classList.add("para-animation");  
 }
 
+const navSlide = () =>{
+    const burger = document.querySelector(".s-burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li"); 
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("links-active");
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = "";
+            }else{
+                link.style.animation = `navLinkFade ease-out forwards ${index / 7 + 1}s`;
+            }
+        });
+        //Cross animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
 
 // Application function handles all that happens in the page
 const applications = () => {
     observer.observe(aboutSection);
+    navSlide();
 }
 applications();
